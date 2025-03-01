a053bbb4932b17eb81649806d9a9a2fa19e564f5
Testcoinn2 <lletechnn@gmail.com>
2025-03-01 15:36:36 +0300

    Add files via upload

---
+++ src/components/layout/index.ts
@@ -0,0 +1,11 @@
+import { Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
+import { Footer } from './Footer/Footer';
+import { Header } from './Header/Header';
+
+export const Layout = {
+  Header,
+  Footer,
+  Breadcrumbs
+};
+
+export default Layout;
